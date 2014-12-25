aa69222ff3100c68395b2855d5e7d0bd805bb092
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2014-12-24 20:15:26 -0500

    begin, end, reset state done

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ var states = { preload: preload,
 var xDim = 1400;
 var yDim = 1000;
 var birdieWeight = 650;
+var FLAP = 420;
 
 var game = new Phaser.Game(xDim, yDim, Phaser.AUTO, '', states);
 
-var birdie;
+var birdie,
+    gameStarted,
+    gameOver;
 
 function preload(){
     game.scale.maxWidth = xDim;
@@ -31,6 +34,7 @@ function create(){
     birdie.anchor.setTo(0.5, 0.5);
     birdie.animations.add('fly', [0, 1, 2, 3], 10, true);
     birdie.scale.setTo(birdieScale, birdieScale);
+    game.input.onDown.add(flap);
 
     // Enable physics on these sprites
     game.physics.enable([birdie], Phaser.Physics.ARCADE);
@@ -42,13 +46,49 @@ function create(){
     reset();
 }
 
+function flap(){
+    if(!gameStarted){
+        beginGame();
+    } else if(!gameOver){
+        birdie.body.velocity.y = -FLAP;
+    } else{
+        reset();
+    }
+}
+
 function reset(){
-	birdie.animations.play('fly', 15, true);
-	birdie.x = game.width / 6;
-	birdie.y = game.height / 2;
-	birdie.body.allowGravity = false;
+	birdie.animations.play('fly', 15, true); 
+    birdie.body.allowGravity = false;
+	birdie.reset(game.world.width / 6, game.world.height / 2);
+    birdie.angle = 0;
+    gameStarted = false;
+    gameOver = false;
+}
+
+function beginGame(){
+    birdie.body.allowGravity = true;
+    gameStarted = true;
+}
+
+function endGame(){
+    birdie.animations.stop('fly');
+    gameOver = true;
+    birdie.angle = 90;
 }
 
 function update(){
+    if(gameStarted){
 
+        var MAX_ANGLE = 60;
+        if (birdie.body.velocity.y <= -FLAP){
+            birdie.angle = -MAX_ANGLE;
+        }else{
+            var angle = MAX_ANGLE / FLAP * birdie.body.velocity.y;
+            birdie.angle = Math.min(angle, 90);
+        }
+
+        if(birdie.body.bottom >= game.world.bounds.bottom){
+            endGame();
+        }
+    }
 }
